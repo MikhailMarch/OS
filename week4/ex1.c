3d16e0ea359a8b69e3f7ef28574d65084364d75c
MikhailMarch <63850348+MikhailMarch@users.noreply.github.com>
2020-09-08 22:20:54 +0300

    Update ex1.c

--- week4/ex1.c
+++ week4/ex1.c
@@ -1,1 +1,13 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h> 
+#include <unistd.h>
+int main(){
+    int n = fork();
+    if(n == 0){
+        printf("Hello from child [%d - %d]\n", getpid(), n );
+    }else if(n > 0){
+        printf("Hello from parent [%d - %d]\n", getpid() , n);
+    }
+    return 0;
+}
